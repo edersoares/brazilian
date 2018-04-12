a32cd4bba5a9451d979e1b917f7487a1f1a90a34
Eder Soares <edersoares@me.com>
2018-04-12 10:34:31 -0300

    Fix problem with installer

--- src/Console/BrazilianInstallCommand.php
+++ src/Console/BrazilianInstallCommand.php
@@ -29,7 +29,7 @@ class BrazilianInstallCommand extends Command
      */
     public function handle()
     {
-        $this->call('migrate --force');
+        $this->call('migrate');
         $this->info('Brazilian states imported: ' . $this->importStates());
         $this->info('Brazilian cities imported: ' . $this->importCities());
     }
