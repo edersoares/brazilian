afb0d4182674ba277da0d084f2d5d85eaa1c6be7
Eder Soares <edersoares@me.com>
2018-04-16 22:58:31 -0300

    Adds relation between state and capital city

--- src/State.php
+++ src/State.php
@@ -21,6 +21,18 @@ class State extends Model
     public $timestamps = false;
 
     /**
+     * Capital city.
+     *
+     * @see \Brazilian\City
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function capital()
+    {
+        return $this->hasOne(City::class, 'id', 'capital_id');
+    }
+
+    /**
      * Cities.
      *
      * @see \Brazilian\City
